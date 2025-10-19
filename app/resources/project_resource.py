fb385bb61d11ab62fcc247e2f55e4d9029f44367
monica <monica.njoki@student.moringaschool.com>
2025-10-19 04:58:32 +0300

    project resourse

--- app/resources/project_resource.py
+++ app/resources/project_resource.py
@@ -4,10 +4,11 @@ Owner: Monica
 Description: CRUD operations for projects + assignment logic
 """
 
-from flask import Blueprint, jsonify, request
-from flask_jwt_extended import get_jwt_identity, jwt_required
-
-project_bp = Blueprint("projects", __name__)
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from app.models.project import Project
+from app.extensions import db
+project_bp = Blueprint('projects', __name__)
 
 # TODO: Monica - Implement project endpoints
 #
