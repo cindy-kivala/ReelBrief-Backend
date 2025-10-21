f82c17a899c9fe9222c3b55541603f385f16f40f
Micheni58 <michenicaleb@gmail.com>
2025-10-21 11:34:28 +0300

    Add sendgrid for mail intergrations

--- app/extensions.py
+++ app/extensions.py
@@ -1,9 +1,11 @@
 from flask_jwt_extended import JWTManager
-from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+import os
+import sendgrid
 
 db = SQLAlchemy()
 migrate = Migrate()
 jwt = JWTManager()
-mail = Mail()
+
+sg = sendgrid.SendGridAPIClient(api_key=os.getenv('SENDGRID_API_KEY'))
