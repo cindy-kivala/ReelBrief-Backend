f6f76002cefefb5a8636f45b87040c52ee8cb166
cindy kivala <cindykivala@gmail.com>
2025-10-22 15:47:05 +0300

    static method decorator

--- app/models/deliverable.py
+++ app/models/deliverable.py
@@ -8,14 +8,6 @@ from datetime import datetime
 
 from app.extensions import db
 
-
-
-# TODO: Cindy - Implement Deliverable model
-#
-
-# Required fields: 
-# - id (Primary Key)
-# - project_id (Foreign Key to projects)
 class Deliverable(db.Model):
     __tablename__ = 'deliverables'
 
@@ -30,11 +22,7 @@ class Deliverable(db.Model):
 # - version_number (Integer, default 1)
     version_number = db.Column(db.Integer, nullable=False, default=1)
 
-# - file_url (Not Null, from Cloudinary)
-# - file_type (image, video, document)
-# - file_size (bytes)
-# - cloudinary_public_id
-# - thumbnail_url (for images/videos)
+# - Files
     file_url = db.Column(db.Text, nullable=False)
     file_type = db.Column(db.String(50))  # 'image', 'video', 'document'
     file_size = db.Column(db.Integer)  # bytes
@@ -57,9 +45,7 @@ class Deliverable(db.Model):
 # Timestamps
     uploaded_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
     reviewed_at = db.Column(db.DateTime, nullable=True)
-# - uploaded_by (Foreign Key to users)
-# - uploaded_at
-# - reviewed_at, reviewed_by (Foreign Key to users)
+
 #
 # Relationships:
 # - deliverable belongs to project
@@ -118,4 +104,28 @@ class Deliverable(db.Model):
         
         return data
 
+    @staticmethod
+    def get_next_version_number(project_id):
+        """Get the next version number for a project"""
+        latest = Deliverable.query.filter_by(project_id=project_id).order_by(Deliverable.version_number.desc()).first()
+        return (latest.version_number + 1) if latest else 1
 
+    def approve(self, reviewed_by_id): #client will be approving after reviewing
+        """Approve this deliverable"""
+        self.status = 'approved'
+        self.reviewed_by = reviewed_by_id
+        self.reviewed_at = datetime.utcnow()
+        db.session.commit()
+
+    def request_revision(self): #Remeberto add this feaure better
+        """Mark deliverable as needing revision"""
+        self.status = 'revision_requested'
+        self.reviewed_at = datetime.utcnow()
+        db.session.commit()
+
+    def reject(self, reviewed_by_id):
+        """Reject this deliverable"""
+        self.status = 'rejected'
+        self.reviewed_by = reviewed_by_id
+        self.reviewed_at = datetime.utcnow()
+        db.session.commit()
